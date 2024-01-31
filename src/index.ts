bd12b2b9ee556c03addea5c0cd3ea59af7756340
MeowSauce <meowsauce@hotmail.com>
2024-02-01 00:50:39 +0800

    Add:New server create.

--- src/index.ts
+++ src/index.ts
@@ -27,12 +27,32 @@ export function apply(ctx: Context) {
     id: 'unsigned',
     servername: 'string',
     serverip: 'string',
-    adddate: 'timestamp',
   })
 
-ctx.command('服务器列表')  .action(({ session }) => {
-  var list = ctx.database.get('palworld_serverlist', [1], ['servername', 'serverip'])
-  session.send('幻兽帕鲁-服务器列表 \n' + String(list) + ' \n收录你的服务器？请发送服务器收录')
-})
+  ctx.command('服务器列表')
+  .action(async ({ session }) => {
+    const list = await ctx.database.get('palworld_serverlist', [1,2,3,4,5], ['servername', 'serverip']);
+    
+    let output = '幻兽帕鲁-服务器列表 \n';
+    for (const item of list) {
+      output += `名称: ${item.servername} | IP: ${item.serverip}\n`;
+    }
+    output += '\n没有收录你的服务器？请发送服务器收录 名称 IP。';
 
-}
+    session.send(output);
+  })
+  var newid = 0
+ctx.command('服务器收录 <name> <ip>')
+  .action(async ({ session },name,ip) => {
+
+    newid = newid+1
+      // 向 servers 表中添加一行数据
+      await ctx.database.create('palworld_serverlist', {
+        id: newid,
+        servername: name,
+        serverip: ip,
+      });
+
+      session.send(`已成功收录服务器：${name} - ${ip}`);
+    }
+  )};
