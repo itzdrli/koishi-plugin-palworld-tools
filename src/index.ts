7dbb699348a18445e8a3f230378c613b6ee40dae
MeowSauce <meowsauce@hotmail.com>
2024-01-31 20:36:53 +0800

    Add main.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,32 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
+declare module 'koishi' {
+  interface Tables {
+    palworld_serverlist: Palworld_ServerList
+  }
+}
+
+// 这里是新增表的接口类型
+export interface Palworld_ServerList {
+  id: number
+  servername: string
+  serverip: string
+  adddate: Date
+}
 export function apply(ctx: Context) {
-  // write your plugin here
+
+  
+  ctx.model.extend('palworld_serverlist', {
+    // 各字段的类型声明
+    id: 'unsigned',
+    servername: 'string',
+    serverip: 'string',
+    adddate: 'timestamp',
+  })
+
+const plugin_id = "d7a4544c-1275-405c-b6c3-32c9164a4319"
+
+ctx.command('/服务器列表')
+  return '当前已收录的服务器有：'
 }
